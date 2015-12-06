f606334032361f5e31cc51d850db10fc2809ce6f
parevalo <Parsif4l>
2015-12-06 18:59:41 -0500

    Initial commit

---
+++ bash_aliases
@@ -0,0 +1,5 @@
+alias login='ssh geo.bu.edu -l parevalo'
+alias tun='ssh parevalo@geo.bu.edu -L'
+alias tpconf='gsettings set org.gnome.settings-daemon.peripherals.input-devices\
+ hotplug-command "/home/paulo/touchpad_settings.sh"'
+
---
+++ touchpad_settings.sh
@@ -0,0 +1,41 @@
+# Helps to reduce mouse cursor jumpiness
+synclient HorizHysteresis=50
+synclient VertHysteresis=50
+
+# Settings reported to work well on an X1 Carbon
+synclient FingerLow=40
+synclient FingerHigh=45
+synclient MinSpeed=1
+synclient MaxSpeed=1
+synclient AccelerationProfile=2
+synclient ConstantDeceleration=4
+synclient VertTwoFingerScroll=1
+synclient VertScrollDelta=75
+synclient HorizTwoFingerScroll=1
+synclient HorizScrollDelta=75   
+
+# Disable edge scrolling, I prefer two-finger scroll instead
+synclient VertEdgeScroll=0
+
+# Disable tap and drag gesture
+synclient TapAndDragGesture=1
+
+# Middle button with two finger CLICK, right click with two finger tap
+synclient ClickFinger2=2
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=0
+
+# Enable Corner buttons
+synclient LTCornerButton=1
+synclient LBCornerButton=1
+synclient RTCornerButton=3
+synclient RBCornerButton=3
+
+#Specify area of right button
+synclient RightButtonAreaLeft=4200
+
+#Others
+synclient PalmDetect=1
+synclient PalmMinWidth=7
+synclient PalmMinZ=40
