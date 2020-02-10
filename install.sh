960020963158904f73aab416e25bac804dc02067
parevalo <--global>
2020-02-10 11:31:32 -0500

    Update installation steps and remove old stuff

--- install.sh
+++ install.sh
@@ -1,79 +1,79 @@
 #!/bin/bash
 ###
 #
-# Install script for Ubuntu (16.04)
+# Install script for Ubuntu (18.04.4)
 # 
-# History:
-#   - 2016/09/09: Copied and modiefied from ceholden
 #
 ###
 
-sudo apt install git
+#sudo apt install git
 
 # SOURCES
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
-sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
 sudo add-apt-repository ppa:numix/ppa
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
-sudo add-apt-repository ppa:mystic-mirage/pycharm
-sudo add-apt-repository ppa:caffeine-developers/ppa
 sudo add-apt-repository -y ppa:rael-gc/rvm
 sudo apt update 
 
 # REPO SOFTWARE
+sudo apt install python-pip
 sudo apt install software-properties-common
-sudo apt install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
-sudo apt install unity-tweak-tool
-sudo apt install indicator-multiload
-sudo apt install libgdal-dev qgis
+#sudo apt install numix-gtk-theme numix-icon-theme numix-icon-theme-circle
+sudo apt install qgis qgis-plugin-grass
 sudo apt install pinta gimp
+sudo apt remove --assume-yes vim-tiny
 sudo apt install vim
 sudo apt install r-base
 sudo apt install remmina
-sudo apt install pycharm-community
 sudo apt install zsh
-sudo apt install python-lxml python-bs4 #Required for the bing wallpaper thing
-sudo apt install python-pip
-sudo apt install caffeine
 sudo apt install rvm
+# Extra packages are for thinkpads only
+sudo apt install tlp tlp-smapi-dkms acpi-call-dkms
+sudo apt install gdebi-core wget
 
 
 # LATEX / TEX
-sudo apt install pandoc texlive-latex-recommended texlive-latex-extra
+#sudo apt install pandoc texlive-latex-recommended texlive-latex-extra
 
-# GOOGLE
-wget http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
-sudo dpkg -i google-earth*.deb
-sudo apt install -f
-
-
-# ANACONDA
+# Skype
 cd /tmp
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-chmod +x miniconda.sh
+wget https://go.skype.com/skypeforlinux-64.deb
+sudo gdebi skypeforlinux-64.deb
 
-./miniconda.sh -b
-export PATH=$HOME/miniconda3/bin:$PATH
+# GOOGLE EARTH
+wget https://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
+sudo gdebi google-earth-pro-stable_current_amd64.deb 
+
+# SLACK
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.3.2-amd64.deb
+sudo gdebi slack-desktop-4.3.2-amd64.deb
+
+# SPOTIFY
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get install spotify-client
+
+# MINICONDA
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b -p $HOME/miniconda
 
 # REPOS
-if [ ! -d $HOME/Documents ]; then
-    mkdir $HOME/Documents
+if [ ! -d $HOME/repos ]; then
+    mkdir $HOME/repos
 fi
-cd $HOME/Documents
+cd $HOME/repos
 
-git clone https://github.com/UtkarshGpta/bing-desktop-wallpaper-changer.git
 git clone https://github.com/powerline/fonts.git
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # SET ZSH AND STUFF
 ./fonts/install.sh
 
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/bashrc ~/.bashrc
-
-# Get powerline for vim (could replace it with vim-airline)
-pip install --user powerline-status
+ln -sf ~/repos/dotfiles/zshrc ~/.zshrc
+ln -sf ~/repos/dotfiles/vimrc ~/.vimrc
+ln -sf ~/repos/dotfiles/bashrc ~/.bashrc
 
 # Reminder:Manuall change config.json to use default_left if using it for shell
 
